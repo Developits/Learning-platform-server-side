afb98400df5c68649dd3d7b43bfa77399fa52370
Developits <nahidnabil1234@gmail.com>
2022-10-27 09:08:50 +0600

    data completed

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const coursesData = require("./data/courses.json");
+
 app.get("/", (req, res) => {
   res.send("Code course api running");
+});
+
+app.get("/coursesInfo", (req, res) => {
+  res.send(coursesData);
+});
+
+app.get("/coursesInfo/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = coursesData.filter((c) => c.id === id);
+  res.send(selectedCourse);
 });
 
 app.listen(port, () => {
